2bdf82b1026263f2cc0cd59ebe19d991a4bad91f
Jay Christopher Anacio <30493303+jcaanacio@users.noreply.github.com>
2023-11-08 17:03:29 +0800

    fix(note): note repository (#20)
    Co-authored-by: Jay Christopher <jayanacio@gmail.com>

--- src/main/java/com/accenture/tigital/repositories/NoteRepository.java
+++ src/main/java/com/accenture/tigital/repositories/NoteRepository.java
@@ -2,6 +2,7 @@ package com.accenture.tigital.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.accenture.tigital.models.Note;
+import java.util.Optional;
 
 public interface NoteRepository extends JpaRepository<Note, Long> {
     Optional<Note> findByUserUserId(Long userId);
