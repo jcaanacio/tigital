36e2a0249c1c588e90570db1bbe64e30e6dc3664
Jay Christopher <jayanacio@gmail.com>
2023-11-13 17:55:26 +0800

    migrations(notes): fix flyway error

--- src/main/resources/db/migration/V2__Drop_Notes.sql
+++ src/main/resources/db/migration/V2__Drop_Notes.sql
@@ -1,6 +1,18 @@
+-- V2__Drop_Notes_Table.sql
+
+-- Remove foreign key reference to avoid conflicts
+ALTER TABLE notes
+DROP CONSTRAINT IF EXISTS fk_notes_user_id;
+
+-- Drop the notes table
+DROP TABLE IF EXISTS notes;
+
+
+
+
 CREATE TABLE notes (
     note_id SERIAL PRIMARY KEY,
-    user_id INT UNIQUE NOT NULL,
+    user_id INT NOT NULL,
     content VARCHAR(255),
     subject VARCHAR(255),
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
