f6881c658e5835097318f1b68e9441673fef4dda
Jay Christopher Anacio <30493303+jcaanacio@users.noreply.github.com>
2023-11-14 18:13:05 +0800

    migration(raffles): recreate raffle migration (#36)
    Co-authored-by: Jay Christopher <jayanacio@gmail.com>

---
+++ src/main/resources/db/migration/V3__Recreate_RaffleSchema.sql
@@ -0,0 +1,11 @@
+
+DROP TABLE IF EXISTS raffles;
+
+CREATE TABLE raffles (
+    raffle_id SERIAL PRIMARY KEY,
+    user_id INT NOT NULL,
+    category VARCHAR(255),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (user_id) REFERENCES users(user_id)
+);
